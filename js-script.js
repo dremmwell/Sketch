f6c07d3256ac21e813433cc4db76af9e7c57da17
dremmwell <dremmwell4>
2022-11-28 20:07:04 +0000

    initial commit + tr/td grid

---
+++ js-script.js
@@ -0,0 +1,21 @@
+function makeGrid(size){
+    const container = document.querySelector('#container');
+    for (let i=0; i < size; i++){
+
+        const row = document.createElement('tr');
+        row.classList.add('row');
+        row.id = "row" + i;
+        // row.textContent = `row ${i+1}`;
+        container.appendChild(row);
+
+        for(let j=0; j< size; j++){
+            const cell = document.createElement('td');
+            cell.classList.add('cell');
+            cell.id = "cell" + i;
+            cell.textContent = `cell ${i+1}`;
+            row.appendChild(cell);
+        }
+    } 
+}
+
+makeGrid(10);
