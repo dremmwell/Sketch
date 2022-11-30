e21ffb8b443d4e468d1fd1e2dab073be447e8922
dremmwell <dremmwell4>
2022-11-30 19:32:16 +0000

    enable click event for each cell

--- js-script.js
+++ js-script.js
@@ -1,21 +1,37 @@
+
+
 function makeGrid(size){
     const container = document.querySelector('#container');
     for (let i=0; i < size; i++){
 
-        const row = document.createElement('tr');
+        const row = document.createElement('div');
         row.classList.add('row');
-        row.id = "row" + i;
+        row.id = "row";
         // row.textContent = `row ${i+1}`;
         container.appendChild(row);
 
         for(let j=0; j< size; j++){
-            const cell = document.createElement('td');
+            const cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.id = "cell" + i;
-            // cell.textContent = `cell ${i+1}`;
+            cell.id = "cell";
+            cell.textContent = `${j+1}-${j+1}`;
             row.appendChild(cell);
         }
     } 
 }
 
+function changeToBlackBackground(div){
+    div.className = "black_bakground";
+}
+
 makeGrid(10);
+
+const cells = document.querySelectorAll("#cell");
+cells.forEach((cell) => {
+    cell.addEventListener("click", function () {
+        console.log("clicked on cell")
+    }, false);
+}) ;
+
+
+ 
